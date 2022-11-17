2b4ed7ed5157b15faedeca7cb444bb750e897217
ivan-linux <kovachivanagustin@gmail.com>
2022-11-17 11:57:52 -0300

    responsive de inicio casi terminado, falta pulir

--- index.js
+++ index.js
@@ -29,12 +29,17 @@ window.onscroll = () => {
   let img = document.querySelectorAll('.img-main')
   const firstScrollTop = 0 - document.getElementById("arduino").getBoundingClientRect().top;
   const secondScrollTop = 0 - document.getElementById("objetivo").getBoundingClientRect().top;
+  const thirdScrollTop = 0 - document.getElementById("aQuienBeneficia").getBoundingClientRect().top;
   if (firstScrollTop < 1) {
     img[2].style.opacity = 1
 
   } else if (secondScrollTop < 0) {
     img[2].style.opacity = 0
     img[1].style.opacity = 1
+  } else if (thirdScrollTop < 0) {
+    img[2].style.opacity = 0
+    img[1].style.opacity = 0
+    img[0].style.opacity = 1
   } 
 
 };
